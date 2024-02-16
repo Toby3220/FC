85f6a25e1efbf502b34bd9bab799370a7af5edcd
Toby3220 <125035715+Toby3220@users.noreply.github.com>
2024-02-16 12:22:47 +0800

    Add files via upload

--- HW1V2.py
+++ HW1V2.py
@@ -8,6 +8,11 @@ from tqdm import tqdm
 
 
 def raw_convert(data:pd.DataFrame,period:int):
+    # convert raw input data into data that samples once every x ticks
+    # data      : pd.DataFrame  - Input Raw Data
+    # period    : int           - sampling period 
+    # return    : pd.DataFrame  - the sampled DataFrame 
+
     sval = period
     out = []
     for i in tqdm(range(0,data.shape[0])):
@@ -22,11 +27,19 @@ def raw_convert(data:pd.DataFrame,period:int):
     return out
         
 def get_ret(data:pd.DataFrame,period:int=1):
-    # this is the rolling return
+    # get n period retruns 
+    # data      : pd.DataFrame  - Input Raw Data
+    # period    : int           - sampling period 
+    # return    : pd.DataFrame  - the sampled DataFrame 
+
     ret =  (data-data.shift(period))/data.shift(period)
     return ret
 
 class data:
+
+    
+    
+    
     # a data object. allow target data and multiple prediective factor set and predictions
     def __init__(self, data: pd.DataFrame, x: list = None, y: str = "Ret") -> None:
         self.datain = data 
@@ -37,13 +50,17 @@ class data:
         self.x = dict()
         self.y = dict()
         self.yhat = dict()
+        
 
         # list of models used
-        self.models_used = set()
+        self.models_used = dict()
+        self.models_status = dict()
+
+        self.fignum=1
 
 
     # split up data seqequence into multiple segments
-    def split(self, periods: dict = {"train": 0.6,"cv":0.2, "test": 0.2}):
+    def split(self, periods: dict = {"train": 0.6,"test": 0.4}):
         po = 0
         valo = 0
         for period in periods.keys():
@@ -57,6 +74,13 @@ class data:
                 self.y[period] = (self.datain[self.y_vec].iloc[valo:valn])
             po = pn
             valo = valn
+
+    def eval_all(self,period:str = "train"):
+        for model in self.models_used.keys():
+            if self.models_status[model]:
+                self.models_used[model].eval(period)
+    
+
             
        
 # empty interface class
@@ -65,25 +89,28 @@ class Model:
         pass
     def create_x():
         pass
+    
     def train():
        pass
+
     def crossval():
         pass
-    def eval(): 
+
+    def eval(self,period:str): 
         pass
 
 
 class EWA_Model_1f(Model):
-
     def __init__(self,data:data,name:str = None) -> None:
         self.data = data
-
+        
         if name:
             self.name = name
         else: 
             self.name = "EWA_Model_1f"      
 
-        data.models_used.add(self.name)
+        data.models_used[self.name]=self
+        data.models_status[self.name]=False
     
     def eval_loss_func(self, sqers, lbd, v0):
         vo = v0
@@ -91,7 +118,7 @@ class EWA_Model_1f(Model):
         lossfunc = self.eval_single_loss(sqers.iloc[0],v0)
         vol = [v0]
         
-        for sqer in sqers.iloc[1:] :
+        for sqer in tqdm(sqers.iloc[1:]) :
             
             # find current vol estimate
             vn = (1-lbd)*sqero+lbd*vo
@@ -124,6 +151,9 @@ class EWA_Model_1f(Model):
         
         self.predict("train")
         print(self.opt_res)
+
+        if self.opt_res["message"]=="Solution found.":
+            self.data.models_status[self.name]=True
     
     def create_x(self, period:str):
         tdata = self.data.y[period]
@@ -145,6 +175,9 @@ class EWA_Model_1f(Model):
         self.data.yhat[period][self.name] = self.volhat
     
     def eval(self,period:str = "test"):
+        plt.figure(self.data.fignum)
+        self.data.fignum+=1
+
         mu = self.data.x[period][self.name][0]
         vhat = self.data.yhat[period][self.name]
         y = pd.DataFrame(self.data.y[period])
@@ -164,8 +197,164 @@ class EWA_Model_1f(Model):
         plt.title("predicted return Intervals vs observed returns: {} period".format(period))
 
         plt.legend()
+        plt.show()
          
+class GarchPQ (EWA_Model_1f):
+    def __init__(self,data:data,name:str = None, P:int = 1, Q: int = 1) -> None:
+        self.data = data
+        self.P = P
+        self.Q = Q
+
+        if name:
+            self.name = name
+        else: 
+            self.name = "Garch{}{}".format      
+
+        data.models_used[self.name]=self
+        data.models_status[self.name]=False
+    
+    def eval_loss_func(self, theta, sqers, v0):
+        # Need to Debug #
+        pvec = theta[:self.P]
+        qvec = theta[self.P:]
+        vol = pd.Series(0,range(0,sqers.shape[0]))
+        vol.iloc[0:v0.shape[0]]=v0
+        lossfunc=0
+
+        sqero = list()
+        volo = list()
+        if self.P > self.Q:
+            sqero[:] = sqers.iloc[:self.P]
+        else:
+            sqero[:] = sqers.iloc[self.Q-self.P:self.Q]
+        volo[:] = v0
+
+        for i in tqdm(range(max(self.P,self.Q),sqers.shape[0])) :
+            # find current vol estimate
+            vn = 0
+            for p in range(1,self.P+1):
+                vn+= pvec[p-1]*sqero[self.P-p]
+            for q in range(1,self.Q+1):
+                vn+= qvec[q-1]*volo[self.Q-q]
+
+            # eval loss for vol
+            lossfunc += self.eval_single_loss(sqers.iat[i],vn)
+            #record estimated vol
+            vol.iat[i] = vn
+
+            # update
+            sqero.pop(0)
+            sqero.append(sqers.iat[i])
+
+            volo.pop(0)
+            volo.append(vn)
+            
+        vol.index=sqers.index
+        self.lossfuncval = lossfunc
+        self.volhat = vol
+        return lossfunc
+
+    def train(self,period:str="train"):
+        # Need to Rework #
+        mu, sqers, v0, theta0 = self.create_x(period)
+        theta0 = theta0.squeeze()
+        # Constraints
+        probA = np.ones([1,len(theta0)])
+        problb = np.zeros(theta0.shape)
+        probub = np.ones(theta0.shape)
+
+        # call optimiser
+        self.opt_res=sp_op.minimize(
+            fun = lambda theta: self.eval_loss_func(theta,sqers=sqers,v0=v0),
+            x0 = theta0,
+            bounds = sp_op.Bounds(lb = problb, ub = probub),
+            constraints= sp_op.LinearConstraint(A =probA, lb = 0, ub=1),
+            options = {
+                "disp":True,
+                "maxiter":500
+            }
+            Jac = 
+            hess =   
+        )
+
+        self.predict("train")
+        print(self.opt_res)
+
+        if self.opt_res["message"]=="Solution found.":
+            self.data.models_status[self.name]=True
+    
+    def create_x(self, period:str, a = 0.99):
+        # Generate estimations
+        tdata = self.data.y[period]
+
+        mu = tdata.mean()
+        vol = tdata.var()
+        sqers = (tdata-mu)**2
+
+        if self.P > self.Q:
+            v0 = sqers.iloc[self.P-self.Q:self.P]
+        else:
+            v0 = sqers.iloc[:self.Q]
+
+        # finding intial guess (estimating assuming v_t = E(v), and Sum(Q)+Sum(P)=1)
+        Mp = pd.DataFrame(index= sqers.index, columns = range(0,self.P))
+        for i in range(1,self.P+1): 
+            Mp.iloc[:,i-1] = sqers.shift(i)
+        Mpones = pd.DataFrame(1,sqers.index,range(0,self.P))
         
+        A = (Mp - vol*Mpones).dropna()
+        b = pd.DataFrame(1,A.index,[0])
+        N = A.T.dot(A)
+        N_inv = pd.DataFrame(np.linalg.pinv(N.values),N.index,N.columns)
+        p0 = N_inv.dot(A.T.dot(b))
+        
+        v_ones_Q1 =pd.DataFrame(1,range(0,self.Q),[0])
+        M_ones_QQ = pd.DataFrame(1, range(0,self.Q), range(0,self.Q))
+        M_ones_QP = pd.DataFrame(1,range(0,self.Q),range(0,self.P))
+        M_ones_QQ_inv = pd.DataFrame(np.linalg.pinv(M_ones_QQ.values),M_ones_QQ.index,M_ones_QQ.columns)
+        
+        q0 = M_ones_QQ_inv.dot(v_ones_Q1)*a- M_ones_QQ_inv.dot(M_ones_QP).dot(p0)
+        theta0 = p0.append(q0).values
+        x = (mu,sqers,v0,theta0)
+        
+        self.data.x[period][self.name]=x
+        
+        return mu,sqers,v0,theta0
+
+    def predict(self,period:str = "test"):
+        thetabest = self.opt_res["x"]
+        mu, sqers, v0, theta0 = self.create_x(period)
+        self.eval_loss_func(theta=thetabest,sqers=sqers,v0=v0)
+
+        self.data.yhat[period][self.name] = self.volhat
+    
+    def eval(self,period:str = "test"):
+        plt.figure(self.data.fignum)
+        self.data.fignum+=1
+
+        mu = self.data.x[period][self.name][0]
+        vhat = self.data.yhat[period][self.name]
+        y = pd.DataFrame(self.data.y[period])
+        stdval = (mu+vhat**0.5).rename(columns = {"vol": "Ret"})
+        less_than_1std = (abs(y)<=stdval).sum()*100/y.shape[0]
+
+        one_std_upper = plt.plot(mu+vhat**0.5,'b', label = "1-std CI")
+        one_std_lower = plt.plot(mu-vhat**0.5,'b')
+
+        actual = plt.plot(y,'g',alpha = 0.5, label = "actual")
+
+        three_std_upper = plt.plot(mu+3*vhat**0.5,'r', label = "3-std CI")
+        three_std_lower = plt.plot(mu-3*vhat**0.5,'r')
+
+        plt.xlabel("sample points \n {}% of samples within 1 std".format(round(less_than_1std.values[0],1)))
+        plt.ylabel("returns")
+        plt.title("predicted return Intervals vs observed returns: {} period".format(period))
+
+        plt.legend()
+        plt.show()
+    
+
+
 # begining of main section
 rawdata = pd.read_csv("hsiapr_1tick.csv")
 rawdata.columns = ["last","number"]
@@ -179,13 +368,19 @@ ret.dropna(inplace=True)
 
 retdata = data(ret)
 retdata.split()
-vol_model = EWA_Model_1f(retdata,"EWA")
-vol_model.train()
-vol_model.predict("test")
-plt.figure(1)
-vol_model.eval("test")
-plt.figure(2)
-vol_model.eval("train")
+# vol_model = EWA_Model_1f(retdata,"EWA")
+# vol_model.train()
+# vol_model.predict("test")
 
+# # evaluate the performance of the model 
+# vol_model.eval("test")
+# vol_model.eval("train")
+
+Garch_model = GarchPQ(retdata,"Garch",P=2,Q=4)
+# mu, sqers, v0, theta0 = Garch_model.create_x("train")
+# Garch_model.eval_loss_func(theta0,sqers,v0)
+Garch_model.train()
+Garch_model.predict("test")
+Garch_model.eval("test")
 
 print("end")
